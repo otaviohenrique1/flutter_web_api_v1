39566b91727128146cf31a99235069719b2a7142
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-06-08 12:48:30 -0300

    commit 1

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_api_v1/screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Journal',
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+      },
+    );
+  }
+}
